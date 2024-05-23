b58cb4118415bbb1d616d9a81c1909de3bdc8482
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-22 20:27:03 -0600

    Create for4.java

---
+++ for4.java
@@ -0,0 +1,16 @@
+
+public class for4 {
+    public static void main(String[] args) {
+       
+        int a;
+        for(a = 1; a >=1; a--){
+            System.out.println("A   A+2   A+4   A+6");
+            System.out.println("3    5     7     9");
+            System.out.println("6    8     10    12");
+            System.out.println("9    11    13    15");
+            System.out.println("12   14    16    18");
+        }
+            
+    }
+    
+}
